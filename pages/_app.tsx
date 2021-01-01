3a08650b140e57551c085aac18504e286518ec8e
she11sh0cked <22623152+she11sh0cked@users.noreply.github.com>
2021-01-02 00:36:02 +0100

    ✨ Add material-ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,30 @@
-import '../styles/globals.css'
+import React, { useEffect } from "react";
+import { AppProps } from "next/dist/next-server/lib/router/router";
+import { CssBaseline, ThemeProvider } from "@material-ui/core";
+import Head from "next/head";
+import theme from "./theme";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function MyApp(props: AppProps): JSX.Element {
+  const { Component, pageProps } = props;
+
+  useEffect(() => {
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) jssStyles.parentElement.removeChild(jssStyles);
+  }, []);
+
+  return (
+    <React.Fragment>
+      <Head>
+        <title>Last FM Playlist</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </React.Fragment>
+  );
 }
-
-export default MyApp
---
+++ pages/_document.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import Document, {
+  DocumentContext,
+  DocumentInitialProps,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from "next/document";
+import { ServerStyleSheets } from "@material-ui/core/styles";
+
+export default class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+
+    const sheets = new ServerStyleSheets();
+    const originalRenderPage = ctx.renderPage;
+
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+      });
+
+    return {
+      ...initialProps,
+      styles: [
+        ...React.Children.toArray(initialProps.styles),
+        sheets.getStyleElement(),
+      ],
+    };
+  }
+
+  render(): JSX.Element {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
